01a4e6b688b4078e3a44ee4d4acecd001a4988b3
puspakirana <puspakiranastv@gmail.com>
2025-03-23 13:40:25 +0700

    project init

---
+++ src/main/java/com/example/belajar_spring_logging/BelajarSpringLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringLoggingApplication.class, args);
+	}
+
+}
