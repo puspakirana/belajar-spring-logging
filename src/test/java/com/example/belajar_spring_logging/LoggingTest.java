07b876f19d368bfe54231475c973d23c1a9d4cf0
puspakirana <puspakiranastv@gmail.com>
2025-03-23 13:52:44 +0700

    Logging Level

--- src/test/java/com/example/belajar_spring_logging/LoggingTest.java
+++ src/test/java/com/example/belajar_spring_logging/LoggingTest.java
@@ -3,9 +3,11 @@ package com.example.belajar_spring_logging;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @Slf4j
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 public class LoggingTest {
 
     @Test
